ed5e6c5400a2c6e7e9f5dd7ae23aec3ce205582e
ayarimatsui <matsui-ayari682@g.ecc.u-tokyo.ac.jp>
2019-11-15 13:41:00 +0900

    modify realtime_accl_graph.py

--- detect_acceleration/realtime_accl_graph.py
+++ detect_acceleration/realtime_accl_graph.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-matplotlibでリアルタイムプロットする例
+matplotlibで加速度センサの値をリアルタイムプロットする
 
-無限にsin関数をplotし続ける
 """
 from __future__ import unicode_literals, print_function
 
@@ -17,10 +16,10 @@ def pause_plot():
     ax1 = plt.subplot(311)
     ax2 = plt.subplot(312)
     ax3 = plt.subplot(313)
-    x = [-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0] #np.arange(-1, 1, 0.1)
-    y_x = [0,0,0,0,0,0,0,0,0,0] #np.sin(x)
-    y_y = [0,0,0,0,0,0,0,0,0,0]
-    y_z = [0,0,0,0,0,0,0,0,0,0]
+    x = [-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0] #はじめに表示されるｘ軸の範囲の設定
+    y_x = [0,0,0,0,0,0,0,0,0,0] #加速度センサのx軸の値の初期値
+    y_y = [0,0,0,0,0,0,0,0,0,0] #加速度センサのy軸の値の初期値
+    y_z = [0,0,0,0,0,0,0,0,0,0] #加速度センサのz軸の値の初期値
     ax1.set_xlim(-1, 0)
     ax1.set_ylim(-1, 1)
     ax2.set_xlim(-1, 0)
@@ -61,7 +60,7 @@ def pause_plot():
 
     # ここから無限にplotする
     while True:
-        # plotデータの更新
+        #加速度センサのデータを代入
         data = bus.read_i2c_block_data(I2C_ADDR, 0x00, 7)
         xAccl = (data[1] * 256 + data[2]) / 16
         if xAccl > 2047 :
@@ -73,43 +72,44 @@ def pause_plot():
         if zAccl > 2047 :
             zAccl -= 4096
 
-        
+        #ｘ軸の更新
         x = map(lambda p: p+0.1, x)
+        
+        #加速度センサｘ軸の値の更新
         y_x.pop(0)
         y_x.append(xAccl)
         
+        #グラフをプロットし直す
         lines1.set_data(x, y_x)
         ax1.set_xlim(min(x), max(x))
         ax1.set_ylim(min(y_x)-10, max(y_x)+10)
 
-        
+
+        #加速度センサy軸の値の更新
         y_y.pop(0)
         y_y.append(yAccl)
         
+        #グラフをプロットし直す
         lines2.set_data(x, y_y)
         ax2.set_xlim(min(x), max(x))
         ax2.set_ylim(min(y_y)-10, max(y_y)+10)
 
         
+        #加速度センサz軸の値の更新
         y_z.pop(0)
         y_z.append(zAccl)
         
+        #グラフをプロットし直す
         lines3.set_data(x, y_z)
         ax3.set_xlim(min(x), max(x))
         ax3.set_ylim(min(y_z)-10, max(y_z)+10)
         
-        
-
-        # set_data()を使うと軸とかは自動設定されないっぽいので，
-        # 今回の例だとあっという間にsinカーブが描画範囲からいなくなる．
-        # そのためx軸の範囲は適宜修正してやる必要がある．
-        #ax.set_xlim(x-1, x)
 
         # 一番のポイント
         # - plt.show() ブロッキングされてリアルタイムに描写できない
         # - plt.ion() + plt.draw() グラフウインドウが固まってプログラムが止まるから使えない
         # ----> plt.pause(interval) これを使う!!! 引数はsleep時間
-        time.sleep(0.1)
+        time.sleep(0.1) #この値によって、何秒に１回センサの値を取得するかが変わる　今は10Hz
         plt.pause(.01)
 
 if __name__ == "__main__":
